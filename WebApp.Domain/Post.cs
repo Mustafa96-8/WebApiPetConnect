e14333cc30f6775a90d56eef5a94671177fdcf58
vildm <vildm@MUSTAFA-ASUSLAP>
2024-08-18 16:02:41 +0500

    Добавьте файлы проекта.

---
+++ WebApp.Domain/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Domain
+{
+    internal class Post
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Breed { get; set; }
+
+        public float Weight { get; set; } // Grams -int or KG -float
+
+        public string Size { get; set; }
+
+    }
+}
